69de1f482391e02046a379854c71d59c0a417abf
Sean Warren <s.warren@garvan.org.au>
2016-01-18 23:20:13 +1100

    Move product into folder for distribution

--- shg-quantification-tools/Compile.m
+++ shg-quantification-tools/Compile.m
@@ -8,6 +8,7 @@ function Compile
     % Get version
     [~,ver] = system('git describe','-echo');
     ver = ver(1:end-1);
+    is_release = isempty(regexp(ver,'-\d-+[a-z0-9]+','ONCE'));
     
     % Build App
     try
@@ -37,6 +38,20 @@ function Compile
         cmd = ['hdiutil create "./build/' new_file '.dmg" -srcfolder ./build/dist/ -volname "' new_file '" -ov'];
         disp(cmd)
         system(cmd)
+        final_file = ['build/' new_file '.dmg'];
+    else
+        final_file = ['build' filesep new_file ext];
     end
+    
+    if is_release
+        dir1 = 'release';
+    else 
+        dir1 = 'latest';
+    end
+    mkdir(['build' filesep dir1]);
+    mkdir(['build' filesep dir1 filesep ver]);
+    
+    movefile(final_file, ['build' filesep dir1 filesep ver]);
+end
     
     

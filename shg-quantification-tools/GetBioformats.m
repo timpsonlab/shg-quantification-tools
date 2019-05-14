93cdc7d2d023b3f9b4e8ff03026f2d55d3127395
Sean Warren <s.warren@garvan.org.au>
2019-05-14 14:55:26 +1000

    Update bioformats

--- shg-quantification-tools/GetBioformats.m
+++ shg-quantification-tools/GetBioformats.m
@@ -2,10 +2,10 @@ function GetBioformats()
 % Add bioformats to path.
 % If required, download from OME and unzip first
 
-    if ~exist('bfmatlab', 'dir')
+    if exist('bfmatlab', 'dir')
         
         disp('Downloading bioformats...');
-        bflink = 'http://downloads.openmicroscopy.org/bio-formats/5.1.7/artifacts/bfmatlab.zip';
+        bflink = 'http://downloads.openmicroscopy.org/bio-formats/5.8.0/artifacts/bfmatlab.zip';
         websave('bfmatlab.zip',bflink);
         unzip('bfmatlab.zip');
         delete('bfmatlab.zip');
